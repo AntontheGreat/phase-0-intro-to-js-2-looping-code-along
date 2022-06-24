66012fb036f42b2c5c3cd529bdcefea6c0fc5e47
Antonio <antonio18casta@aol.com>
2022-06-24 15:44:25 -0400

    commit tree dff3d8c0babd183f9eb7f60f755fdfd3f881a8a2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event) {
+    let message = [];
+    for (let i = 0; i < cards.length; i++) {
+        message.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return message;
+}
+
+function countDown() {
+    for (let countDown = 10; countDown > -1; countDown--) {
+        console.log(countDown);
+    }
+}
